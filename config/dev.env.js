92f72745d463384c55918789d4102f4735407fba
twodogwang <a381097999@qq.com>
2018-01-31 18:16:29 +0800

    全局output

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,10 +1,18 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+console.log(prodEnv)
 
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_URL: "'http://192.168.0.108:8360/blog'",// 在家
-  BASE_URL: '"http://192.168.88.219:8360/blog"'// 在公司
+  BASE_URL: '"http://192.168.88.219:8360/blog"', // 在公司
+  /* output: function (xiaoyang) {
+    if (this !== undefined) {
+      console.log(`${xiaoyang}                   <=== from ${this.$route.name}.vue`);
+    } else {
+      console.log(xiaoyang)
+    }
+  } */
 })
